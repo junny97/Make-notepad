04b5cec34ebc2c5252fd83029979ffedac9f63c8
junny97 <pcohad12@gmail.com>
2023-04-24 21:14:55 +0900

    메모지 생성 함수 분리

--- my.js
+++ my.js
@@ -1,21 +1,19 @@
 const addNoteBtn = document.querySelector(".add-note-btn");
 const memoContainer = document.querySelector(".memo-container");
 
-
-// "data"라는 key를 가진 값이 있으면 그 값을 JSON.parse를 통해 배열 형태로 파싱하여 data 변수에 할당하고, 값이 없는 경우 빈 배열을 할당합니다.
 let data = JSON.parse(localStorage.getItem("data")) || [];
-//  메모 데이터를 저장할 배열
-//  let data = [];
-//  if(localStorage.getItem("data")){
-//     data = JSON.parse(localStorage.getItem("data"));
-//  }
 
-//메모추가 버튼 클릭시 포스트잇 메모지 나타나게 만드는 코드 
-addNoteBtn.addEventListener("click", () => {
+
+// 포스트잇 메모지 생성 함수 분리
+function createMemo(memoTextContent) {
+
+  //fragment를 사용하면 여러 번 DOM에 추가되는 것이 아니라 한 번에 추가가 가능하다
+  const fragment = new DocumentFragment();
+
   const memo = document.createElement("div");
   memo.className = "memo";
   memo.innerHTML = `
-    <textarea class="memo-textContent"></textarea>
+    <textarea class="memo-textContent">${memoTextContent}</textarea>
     <div class="add-remove-Btn"> 
       <button class="submit-btn" type="button">작성완료</button>
       <button class="delete-btn" type="button">삭제하기</button>
@@ -26,10 +24,10 @@ addNoteBtn.addEventListener("click", () => {
     </div>
   `;
 
-  memoContainer.appendChild(memo);
-
-
-  //포스트잇 css 속성
+  fragment.appendChild(memo);
+  memoContainer.appendChild(fragment);
+  memoContainer.classList.add("memo-container-change");
+  // memoContainer.appendChild(memo);
   memoContainer.style.display = "grid";
   memoContainer.style.gridAutoRows = "max-content";
   memoContainer.style.gridTemplateColumns = "repeat(auto-fill, minmax(300px, 1fr))";
@@ -42,19 +40,16 @@ addNoteBtn.addEventListener("click", () => {
   //   memo.classList.toggle("pin");
   // });
 
-  
-
   const submitBtn = memo.querySelector(".submit-btn");
   submitBtn.addEventListener("click", () => {
     const memoTextContent = memoTextarea.value.trim();
     if (memoTextContent) {
       data.push(memoTextContent);
       localStorage.setItem("data", JSON.stringify(data));
-      alert("메모가 성공적으로 저장되었습니다!")
-    //   memoTextarea.value = "";
-    }else{
+      alert("메모가 성공적으로 저장되었습니다!");
+    } else {
       alert("메모를 작성해주세요!");
-      retrun
+     
     }
   });
 
@@ -63,14 +58,35 @@ addNoteBtn.addEventListener("click", () => {
     const confirmed = confirm("정말 메모를 삭제하시겠습니까?");
     if (confirmed) {
       memo.remove();
-      localStorage.removeItem("data");
+      const memoTextContent = memoTextarea.value.trim();
+      const index = data.indexOf(memoTextContent);
+      if (index!== -1) {
+        data.splice(index, 1);
+        localStorage.setItem("data", JSON.stringify(data));
+      }
     }
   });
 
-  // const collapseBtn = memo.querySelector(".collapse-btn");
-  // collapseBtn.addEventListener("click", () => {
-  //   memo.classList.toggle("collapsed");
-  // });
 
+  const collapseBtn = memo.querySelector(".collapse-btn");
+  collapseBtn.addEventListener("click", () => {
+    memo.classList.toggle("collapsed");
+  });
+}
+
+// 저장된 메모 데이터를 이용해서 초기화면에 렌더링하는 함수 
+function renderMemos() {
+  data.forEach((memoTextContent) => {
+    createMemo(memoTextContent);
+  });
+}
+
+// 초기화면 렌더링
+renderMemos();
+
+
+addNoteBtn.addEventListener("click", () => {
+  createMemo("");
 });
+
 
